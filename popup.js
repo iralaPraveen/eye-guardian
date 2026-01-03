d734a55e32e188437016d303968f80cd9b66cedd
irala praveen <iralapraveenofficial@gmail.com>
2026-01-03 22:08:21 +0530

    user actions

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+  console.log("START clicked");
+});
+
+document.getElementById("stopBtn").addEventListener("click", () => {
+  console.log("STOP clicked");
+});
